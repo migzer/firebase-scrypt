20375caa6e6dbfad58f65caed67eb884e3f2ace4
Laurie Louvet <contact@gmardon.com>
2019-08-15 14:08:04 +0200

    adapt installation script to macos env

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 echo '  [x] Install SLL dev dependency'
-sudo apt install libssl-dev 
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        sudo apt install libssl-dev
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        sudo brew install openssl 
 
 echo '  [x] Install automake'
-sudo apt install automake
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	sudo apt install automake
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	sudo brew install automake
 
 echo '  [x] Clone firebase Scrypt repo'
 git clone https://github.com/firebase/scrypt.git
