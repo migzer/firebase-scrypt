422def8b5f61697789d876aa7a8abf10f2a034c2
Laurie Louvet <contact@gmardon.com>
2019-08-15 14:14:03 +0200

    error syntax bash

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
         sudo apt install libssl-dev
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         sudo brew install openssl 
+fi
 
 echo '  [x] Install automake'
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	sudo apt install automake
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	sudo brew install automake
+fi
 
 echo '  [x] Clone firebase Scrypt repo'
 git clone https://github.com/firebase/scrypt.git
